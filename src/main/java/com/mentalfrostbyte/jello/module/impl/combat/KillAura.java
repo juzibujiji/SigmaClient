ac14aff2ad7557b4f7128a76279dd440c73e637d
juzibujiji <1590079027@qq.com>
2026-07-17 18:27:38 +0800

    better>?

--- src/main/java/com/mentalfrostbyte/jello/module/impl/combat/KillAura.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/combat/KillAura.java
@@ -833,7 +833,7 @@ public class KillAura extends Module {
                     List<LivingEntity> raytracelist = new ArrayList<>();
                     if (this.getBooleanValueFromSettingName("Raytrace")) {
                         //EntityRayTraceResult result = this.rayTraceWithKillAuraRotation(entity);
-                        raytracelist = BlockUtil.rayTraceEntities(RotationCore.lastYaw,RotationCore.lastPitch,3.0f,this.getBooleanValueFromSettingName("Through walls"));
+                        raytracelist = BlockUtil.rayTraceEntities(RotationCore.currentYaw,RotationCore.currentPitch,3.0f,this.getBooleanValueFromSettingName("Through walls"));
                         if (this.getBooleanValueFromSettingName("RaytraceMultiAttack")) {
                             if (raytracelist.isEmpty()) {
                                 canAttack = false;
