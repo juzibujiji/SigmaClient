2a6cd30444314e5480d245c4206b5dd6bf0a5f66
Zis30axs <call13636567127@126.com>
2026-08-17 23:37:55 +0800

    恢复 CorrectMovement 的 Vape 模式

--- src/main/java/com/mentalfrostbyte/jello/module/impl/movement/CorrectMovement.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/movement/CorrectMovement.java
@@ -3,6 +3,7 @@ package com.mentalfrostbyte.jello.module.impl.movement;
 import com.mentalfrostbyte.jello.module.data.ModuleCategory;
 import com.mentalfrostbyte.jello.module.data.ModuleWithModuleSettings;
 import com.mentalfrostbyte.jello.module.impl.movement.crtmov.Sigma;
+import com.mentalfrostbyte.jello.module.impl.movement.crtmov.Vape;
 import com.mentalfrostbyte.jello.module.impl.movement.crtmov.Zen;
 import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
 public class CorrectMovement extends ModuleWithModuleSettings {
@@ -11,6 +12,7 @@ public class CorrectMovement extends ModuleWithModuleSettings {
                 "CorrectMovement",
                 "juzi suck my dick" ,
                 new Zen(),
+                new Vape(),
                 new Sigma()
         );
 
